6dd301b5e346457047b0a90f1f09a4851289c5c7
Kacper Kotecki <kacperkotecki@o2.pl>
2025-11-10 15:10:46 +0100

    Initial commit: Add project structure and .gitignore

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Invoice_Manager.Startup))]
+namespace Invoice_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
